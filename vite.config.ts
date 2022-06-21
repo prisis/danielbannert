148a72fdfdd9445785cbdb8f42c6dd3bf7b0325d
prisis <d.bannert@anolilab.de>
2022-06-21 16:26:15 +0200

    fix: fixed favicon loading

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,21 @@ import react from "@vitejs/plugin-react";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig, loadEnv } from "vite";
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { ViteFaviconsPlugin } from "vite-plugin-favicon";
+import { ViteFaviconsPlugin } from "vite-plugin-favicon2";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import ssr from "vite-plugin-ssr/plugin";
 
 export default defineConfig(async ({ mode }) => {
     Object.assign(process.env, loadEnv(mode, process.cwd()));
 
-    const productionPlugins = [];
-
-    if (mode === "production") {
-        productionPlugins.push(
+    return {
+        plugins: [
+            react(),
+            ssr(),
             ViteFaviconsPlugin({
                 logo: "assets/favicon.svg",
+                inject: false,
             }),
-        );
-    }
-
-    return {
-        plugins: [react(), ssr(), ...productionPlugins],
+        ],
     };
 });
